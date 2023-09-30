0ef77dc4e8f0461012ec9b5465ca3552a01a9327
7nischal <112294161+7nischal@users.noreply.github.com>
2023-09-30 15:54:53 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/*
+  Student's Name: Nischal Adhikari
+  Student ID: 301301600
+  Date: 2023-09-30
+*/
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -9,16 +15,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Define your routes
-app.get('/', (req, res) => {
+app.get('home.ejs', (req, res) => {
   // Render the Home Page using the 'home.ejs' template
   res.render('home');
 });
 
-app.get('/contact', (req, res) => {
+app.get('contact.ejs', (req, res) => {
   // Render the Contact Me page using the 'contact.ejs' template
   res.render('contact');
 });
 
+app.get('projects.ejs', (req, res) => {
+  // Render the Contact Me page using the 'contact.ejs' template
+  res.render('project');
+});
+app.get('service.ejs', (req, res) => {
+  // Render the Contact Me page using the 'contact.ejs' template
+  res.render('service');
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
